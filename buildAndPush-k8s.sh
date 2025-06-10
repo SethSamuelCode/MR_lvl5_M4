b496c978449401386dde326bd1dca4dcff0cd49d
Seth Samuel <Seths@missionreadyhq.com>
2025-06-10 23:24:52 +0000

    add build and push scripts for frontend and backend Docker images to work with k8s

---
+++ buildAndPush-k8s.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+frontend=git.sethsamuel.online/fluffy/mr_lvl5_m4_front:k8s
+backend=git.sethsamuel.online/fluffy/mr_lvl5_m4_back
+
+docker build . -f front.k8s.dockerfile -t $frontend
+docker build . -f back.dockerfile -t $backend
+
+docker push $frontend
+docker push $backend
+
---
+++ front.k8s.dockerfile
@@ -0,0 +1,6 @@
+FROM nginx:alpine
+COPY ./frontend /usr/share/nginx/html
+RUN sed -i 's#http//localhost:8000#http://backm4.fluffyb.net#'
+EXPOSE 80
+# Start Container
+CMD ["nginx", "-g", "daemon off;"]
